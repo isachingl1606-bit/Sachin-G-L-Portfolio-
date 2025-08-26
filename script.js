3b351f89f74c8aa38454835322b2e6f62190d8f6
isachingl1606-bit <isachingl.1606@gmail.com>
2025-08-26 15:55:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple form validation
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  alert("Thank you for reaching out, Sachin will contact you soon!");
+
+});
